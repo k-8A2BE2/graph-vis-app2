2c8238d87ffd6e6628df69a6802f2b9a640c4b88
akiyama <akiyama@localhost.localdomain>
2020-11-12 14:35:32 +0900

    Change Port: construct server in localhost:8080

--- server.py
+++ server.py
@@ -26,5 +26,5 @@ def favicon():
     return app.send_static_file('./material/favicon.ico')
 
 if __name__ == "__main__":
-    # app.run(host='127.0.0.1', port=8000, debug=True)
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
+    # app.run(host='0.0.0.0', port=80, debug=True)
